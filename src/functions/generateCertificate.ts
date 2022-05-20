a8e867af0d055c74c76926ae8cae76f47cc8b430
pdr3392 <pchenr1que1@gmail.com>
2022-05-20 04:48:51 -0300

    s3 integrated

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -4,6 +4,7 @@ import { compile } from "handlebars";
 import { join } from "path";
 import { readFileSync } from "fs";
 import dayjs from "dayjs";
+import { S3 } from "aws-sdk";
 import chromium from "chrome-aws-lambda";
 
 interface ICreateCertificate {
@@ -31,18 +32,6 @@ const compileTemplate = async (data: ITemplate) => {
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  await document
-    .put({
-      TableName: "users_certificate",
-      Item: {
-        id,
-        name,
-        grade,
-        created_at: new Date().getTime(),
-      },
-    })
-    .promise();
-
   const response = await document
     .query({
       TableName: "users_certificate",
@@ -52,6 +41,22 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const userAlreadyExists = response.Items[0];
+
+  if (!userAlreadyExists) {
+    await document
+      .put({
+        TableName: "users_certificate",
+        Item: {
+          id,
+          name,
+          grade,
+          created_at: new Date().getTime(),
+        },
+      })
+      .promise();
+  }
 
   const medalPath = join(process.cwd(), "src", "templates", "selo.png");
   const medal = readFileSync(medalPath, "base64");
@@ -86,8 +91,29 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
   await browser.close();
 
+  const s3 = new S3();
+
+  /* await s3
+    .createBucket({
+      Bucket: "certificateignitepedro",
+    })
+    .promise(); */
+
+  await s3
+    .putObject({
+      Bucket: "certificateignitepedro",
+      Key: `${id}.pdf`,
+      ACL: "public-read",
+      Body: pdf,
+      ContentType: "application/pdf",
+    })
+    .promise();
+
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0]),
+    body: JSON.stringify({
+      message: "Certificado criado com sucesso",
+      url: `https://certificateignitepedro.s3.amazonaws.com/${id}.pdf`,
+    }),
   };
 };
